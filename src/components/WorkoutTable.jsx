68ef5864e400f8a4a0e0df77745a9b56dfcbb95f
Chris Nelson <chrisnelson7791@gmail.com>
2025-04-25 17:55:36 -0700

    feat: begin styling WorkoutTable component.

--- src/components/WorkoutTable.jsx
+++ src/components/WorkoutTable.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
+import { StyledWorkoutTable } from './styles/WorkoutTable.styled'
 
 const WorkoutTable = () => {
 
 
   return (
-    <>
+    <StyledWorkoutTable>
       <table>
         <thead>
-          <tr>
-            <th>Breathe Up</th>
+          <tr className="categories">
+            <th>Breathe</th>
             <th>Hold</th>
+            <th>Actual</th>
+          </tr>
+          <tr>
+            <th>set 1.breathe</th>
+            <th>set 1.hold</th>
+            <th>set 1.holdActual</th>
+          </tr>
+          <tr>
+            <th>set 2.breathe</th>
+            <th>set 2.hold</th>
+            <th>set 2.holdActual</th>
           </tr>
         </thead>
       </table>
-    </>
+    </StyledWorkoutTable>
   )
 }
 
--- src/components/styles/WorkoutTable.styled.jsx
+++ src/components/styles/WorkoutTable.styled.jsx
@@ -1,6 +1,16 @@
 import styled from 'styled-components'
 
-export const StyledWorkoutTable = styled.table`
+export const StyledWorkoutTable = styled.section`
 
+  margin: 1rem;
+  border: solid 2px gray;
+  border-radius: 5px;
+
+  tr {
+    font-size: 1rem;
+  }
+  tr.categories {
+    font-size: 1.15rem;
+  }
 
 `
